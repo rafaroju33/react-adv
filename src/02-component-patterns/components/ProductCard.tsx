4078298369404f90c4c25dddafd8cacffbf94aed
rafaelturn <rafael.rodriguez@turn.ai>
2022-11-28 15:08:43 -0500

    Component Extend Styles listo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,14 +1,21 @@
-import { createContext, ReactElement, useContext } from "react";
+import { createContext, CSSProperties, ReactElement } from "react";
 import styles from "../styles/styles.module.css";
 
 import { useProduct } from "../hooks/useProduct";
-import { ProductCardProps, ProductContextProps } from "../interfaces/interfaces";
+import { Product, ProductContextProps } from "../interfaces/interfaces";
 
 
 export const ProductContext = createContext({} as ProductContextProps);
 const {Provider} = ProductContext;
 
-export const ProductCard = ({children, product}: ProductCardProps) => {
+export interface Props {
+  product: Product,
+  children?: ReactElement | ReactElement[];
+  className?: string;
+  style?: CSSProperties
+}
+
+export const ProductCard = ({children, product, className, style}: Props) => {
     
   const {counter, increaseBy} = useProduct();
 
@@ -18,7 +25,10 @@ export const ProductCard = ({children, product}: ProductCardProps) => {
       increaseBy,
       product
     }}>
-      <div className={styles.productCard}>
+      <div 
+        className={`${styles.productCard} ${className}`}
+        style={style}
+      >
         {/* <img className={styles.productImg} src="./coffee-mug.png" alt="Coffee Mug" /> */}
 
         {children}
